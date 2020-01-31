e8f425c2991db520262db014359a9b0abe5a54f6
Daniel <fireblastdaniel@yahoo.com>
2020-01-31 11:17:48 -0800

    added styled component and answered self-study

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,13 +1,22 @@
 import React from "react";
+import styled from 'styled-components';
 import './components.css';
 
 const CharacterCard = props => {
+  const CharCard = styled.div`
+    width: 28%;
+    border: 1px solid black;
+    border-radius: 8px;
+    margin: 1%;
+    padding: 1%;
+  `;
+
   return (
-    <div className='char-card'>
+    <CharCard>
       <h2 className='name'   >{props.info.name}</h2>
       <h3 className='species'>{props.info.species}</h3>
       <h3 className='origin' >{props.info.origin.name}</h3>
-    </div>
+    </CharCard>
   );
 }
 
